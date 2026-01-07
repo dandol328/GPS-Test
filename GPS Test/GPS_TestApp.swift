dbeb6a5968c79ad001673be7753c94fab2e9babe
dandol328 <dandol328@gmail.com>
2026-01-06 23:26:55 -0700

    Initial Commit

---
+++ GPS Test/GPS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPS_TestApp.swift
+//  GPS Test
+//
+//  Created by Dan on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct GPS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPS TestTests/GPS_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GPS_TestTests.swift
+//  GPS TestTests
+//
+//  Created by Dan on 1/6/26.
+//
+
+import Testing
+@testable import GPS_Test
+
+struct GPS_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
